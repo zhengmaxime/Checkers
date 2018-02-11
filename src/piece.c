0afe7202767cccddbc6396a81ca39e6f61db4b1e
Maxime Zheng <maxime.zheng@epita.fr>
2018-02-11 17:49:36 +0100

    add fn in piece.c. git checkout otherbranch file to move files between branches

--- src/piece.c
+++ src/piece.c
@@ -8,3 +8,15 @@ int is_pawn(int piece){
 int is_king(int piece){
   return (piece == BK || piece == WK);
 };
+
+int is_white(int piece){
+  return (piece == WP || piece == WK);
+}
+
+int is_black(int piece){
+  return (piece == BP || piece == BK);
+}
+
+int is_out_of_board(int x, int y){
+  return (x < 0 || y < 0 || x > 9 || y > 9);
+}
--- src/piece.h
+++ src/piece.h
@@ -1,2 +1,5 @@
 int is_pawn(int piece);
 int is_king(int piece);
+int is_white(int piece);
+int is_black(int piece);
+int is_out_of_board(int x, int y);
