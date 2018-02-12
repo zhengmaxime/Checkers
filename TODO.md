b28b07746c6e903e0f637e34de7783c8c6c2dc91
Maxime Zheng <maxime.zheng@epita.fr>
2018-02-12 01:20:30 +0100

    sanitize inputs, add quit and help commands

--- TODO.md
+++ TODO.md
@@ -1,12 +1,16 @@
 ### Jeu de dames
 
 - [x] Mode 2 joueurs
-- [ ] Vérification saisie
+- [X] Vérification saisie
+- [x] Comptage pieces
 - [x] Affichage console
-- [ ] Affichage graphique
+- [ ] Affichage graphique SDL
 - [x] Gestion d'erreur pour déplacement sans prise
 - [ ] Trouver les coups obligatoires (avec prise)
     - [ ] Prise simple
     - [ ] Rafle
 - [ ] Transformation en dame
 - [ ] Fin de partie (victoire/défaite)
+
+- [ ] Site Web
+- [ ] Rapport de soutenance
--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include "board.h"
 # include "piece.h"
 
+void fflush_stdin()
+{
+  int c = 0;
+  while (c != '\n' && c != EOF)
+    c = getchar();
+}
+
+int parse_input(int *curLine, int *curCol, int *destLine, int *destCol)
+{
+  char input[20] = {0};
+
+  if (fgets(input, 20, stdin) != NULL) // reads 18 char + \n, store to input
+  {
+    if (strchr(input, '\n') == NULL) // input too long, didn't find \n
+      fflush_stdin(); // prevent overflow
+
+    if (strncmp(input, "quit", 4) == 0)
+      return 1;
+
+    if (strncmp(input, "help", 4) == 0)
+      return 2;
+
+    if (input[0] >= 48 && input[0] <= 57)
+    {
+      sscanf(input, "%d %d %d %d", curLine, curCol, destLine, destCol);
+      return 0;
+    }
+
+    return -1; // error
+  }
+  else
+    return -1; // error
+}
 
 int main()
 {
@@ -13,22 +47,55 @@ int main()
   //print it
   printf("This is the start of the game\n");
   printBoard(board);
-  int curLine = 0;
-  int curCol = 0;
-  int destLine = 0;
-  int destCol = 0;
-  //main loop
-  for(;;)
-  {
-    scanf("%d %d %d %d", &curLine, &curCol, &destLine, &destCol);
-    /* consume all the char remaining in stdin buffer
-       prevent overflow and infinite loop */
-    while (getchar() != '\n');
 
-    if (0 == deplacement(board, curLine, curCol, destLine, destCol))
-      board->player *= -1;
-    printBoard(board);
+  int *curLine, *curCol, *destLine, *destCol;
+  curLine  = malloc(sizeof (int));
+  curCol   = malloc(sizeof (int));
+  destLine = malloc(sizeof (int));
+  destCol  = malloc(sizeof (int));
+
+  int res;
+
+  //main loop
+  for (;;)
+  {
+    res = parse_input(curLine, curCol, destLine, destCol);
+
+    if (res == -1) //error
+    {
+      puts("Problem when reading your input");
+      continue;
+    }
+
+    if (res == 0) //normal
+    {
+      if (0 == deplacement(board, *curLine, *curCol, *destLine, *destCol))
+        board->player *= -1;
+      printBoard(board);
+    }
+
+    if (res == 1) //quit
+      break;
+
+    if (res == 2) //help
+    {
+      puts("Type 4 digits separated by space character:"
+        " current line and column, destination line and column");
+      continue;
+    }
   }
+
   free(board);
+  free(curLine);
+  free(curCol);
+  free(destLine);
+  free(destCol);
   return 0;
 }
+
+/*
+scanf("%d %d %d %d", &curLine, &curCol, &destLine, &destCol);
+// consume all the char remaining in stdin buffer
+// prevent overflow and infinite loop
+while (getchar() != '\n');
+*/
