c6b7f7636b14cf26df6dfc7f55e7e609bfd9f218
Maxime Zheng <maxime.zheng@epita.fr>
2018-03-27 22:09:04 +0200

    add macros for commands

--- src/constants.h
+++ src/constants.h
@@ -1,10 +1,17 @@
-#ifndef DEF_CONSTANTES
-#define DEF_CONSTANTES
+#ifndef DEF_CONSTANTS
+#define DEF_CONSTANTS
 
 #define BLOCK_SIZE          75 // square, in pixels
 #define NB_BLOCKS_WIDTH     10
 #define NB_BLOCKS_HEIGHT    10
 #define SCREEN_WIDTH     BLOCK_SIZE * NB_BLOCKS_WIDTH
 #define SCREEN_HEIGHT    BLOCK_SIZE * NB_BLOCKS_HEIGHT
+
+#define QUIT 1
+#define HELP 2
+#define SAVE 3
+#define UNDO 4
+#define REDO 5
+#define SWITCH_TO_MOUSE 6
 
 #endif
