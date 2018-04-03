5ea54402883fb6b2eee6c437ca277a8aebd42cc9
Adrien Nebon-Carle <adrien.nebon-carle@epita.fr>
2018-04-03 11:50:39 +0200

    IA ramdom move WORKS

--- src/IA.c
+++ src/IA.c
@@ -14,9 +14,43 @@
 # include "simple_move.h"
 # include "history.h"
 # include "IA.h"
+# include <time.h>
 
 struct move_seq *get_IA_move(struct board *b)
 {
+  struct moves *moves_list = NULL;
+  unsigned int mandatory_jumps;
+	int move_choice, moves_list_len;
+
+  moves_list = build_moves(b);
   
-  return NULL;
+  mandatory_jumps = list_len(moves_list);
+
+  if(mandatory_jumps > 0)
+  {
+  }
+  else
+  {
+    free_moves(moves_list);
+    moves_list = build_moves_not_mandatory(b);
+	}
+  moves_list_len = list_len(moves_list);
+
+	if (moves_list_len == 0)
+    return NULL;
+  
+  srand(time(NULL));
+  move_choice = rand() % moves_list_len;
+
+	moves_list = moves_list->next; //sentinel jumped
+	
+  for(int i = 0; i < move_choice; i++)
+	{
+		moves_list = moves_list->next;
+	}
+  
+  if (moves_list == NULL)
+    return NULL;
+	
+  return moves_list->seq;
 }
